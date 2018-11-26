b8be0a198c761406c82f12783975801d74109228
minji <ssspmj1204@naver.com>
2018-11-26 18:09:30 +0900

    sorce6

---
+++ 파일 분할/AccountHandler.h
@@ -0,0 +1,23 @@
+#ifndef __ACCOUNT_HANDLER_H__
+#define __ACCOUNT_HANDLER_H__
+
+#include "Account.h"
+
+class AccountHandler {
+private:
+	Account *accArr[100];
+	int accNum;
+protected:
+	void MakeNormalAccount(void);
+	void MakeCreditAccount(void);
+public:
+	AccountHandler();
+	void ShowMenu(void) const;
+	void MakeAccount(void);
+	void DepositMoney(void);
+	void WithdrawMoney(void);
+	void ShowAllAccInfo(void) const;
+	~AccountHandler();
+};
+
+#endif
